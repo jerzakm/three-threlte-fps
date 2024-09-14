55974ec06c6eafef2ef0a533566960fb32f23ed4
Marcin Jerzak <jerzakm@gmail.com>
2024-09-14 19:47:57 +0200

    debug

--- src/lib/debug/debugStores.ts
+++ src/lib/debug/debugStores.ts
@@ -1,6 +1,7 @@
+import { dev } from '$app/environment';
 import { writable } from 'svelte/store';
 
-const debugActive = writable(true);
+const debugActive = writable(dev);
 
 export const debugStores = {
 	debugActive
